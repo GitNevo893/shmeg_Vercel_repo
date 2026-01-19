150f43f6aa4d533f28f71eb8520144cccbec55f2
GitNevo893 <warrior.sharkies@gmail.com>
2026-01-19 18:29:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-const signalingUrl = "wss://YOUR-RENDER-SIGNALING-SERVER";
+console.log("main.js loaded");
 
 const button = document.getElementById("toggleBtn");
 
-button.onclick = async () => {
-  console.log("WebRTC agent starting...");
-
-  // later:
-  // - create RTCPeerConnection
-  // - capture microphone
-  // - connect to signaling server
-};
+button.addEventListener("click", () => {
+  console.log("Start button clicked");
+});
