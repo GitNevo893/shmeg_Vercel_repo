5d294d338bad120268768f8f6374249690fc11c6
GitNevo893 <warrior.sharkies@gmail.com>
2026-01-19 15:29:55 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const signalingUrl = "wss://YOUR-RENDER-SIGNALING-SERVER";
+
+const button = document.getElementById("toggleBtn");
+
+button.onclick = async () => {
+  console.log("WebRTC agent starting...");
+
+  // later:
+  // - create RTCPeerConnection
+  // - capture microphone
+  // - connect to signaling server
+};
